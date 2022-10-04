6c0c0a22dc205532c584559478e486161ae8edca
ClaudioChametla <clauchametla.777@gmail.com>
2022-10-04 15:00:02 -0500

    Modificado hoja de respuestas
    La hora de respuestas es dinámica y cambia según el cuestionario realizado

--- src/app/components/luna/luna.component.ts
+++ src/app/components/luna/luna.component.ts
@@ -24,7 +24,7 @@ export class LunaComponent implements OnInit{
   random:number=0;
 
   //Para mostrar u ocultar la pizarra
-  hiddenGame:Boolean=true;
+  hiddenGame:Boolean=false;
 
 
   constructor(private router:Router) { }
@@ -34,11 +34,13 @@ export class LunaComponent implements OnInit{
     //Selecciona un numero aleatorio entre 1 y 4
     //Para seleccionar el cuestionario
     this.random = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+
   }
 
   //oculta la pizarra
   salir(){
     this.hiddenGame=false;
+    this.puntaje=0;
   }
 
   //Muestra el cuestionario
@@ -103,61 +105,103 @@ export class LunaComponent implements OnInit{
     this.router.navigate(['/sistema-solar']);
   }
 
+  //Metodo para obtener la hoja de respuestas
+  private obtenerHoja(ran:number){
+    let answer=[];
+    for (const element of this.lunaDatos[ran]) {
+      if(element.opciones[0].value==1){
+        answer.push(element.opciones[0].opcion)
+      }else if(element.opciones[1].value==1){
+        answer.push(element.opciones[1].opcion)
+      } else if(element.opciones[2].value==1){
+        answer.push(element.opciones[2].opcion)
+      }
+    }
+    for (const element of this.lunaDatos[ran]) {
+      answer.push(element.pregunta);
+    }
+    return answer;
+  }
+
   //Este metodo crea u PDF con los resultados obtenidos
   imprimirResultados(){
-    //Si el puntaje obtenido es mayor a 8 desbloquea la pizarra
-    if(this.puntaje>8){
-      this.hiddenGame=true;
+    let mensajePuntaje: string = '';
+
+    //Si el puntaje obtenido es mayor a 9 desbloquea la pizarra
+    //Mensaje cambia segun la puntuacion
+    switch (this.puntaje) {
+      case 10:
+      case 9:
+
+        this.hiddenGame=true;
+        mensajePuntaje='¡¡¡ Muchas felicidades !!!, Excelente trabajo';
+        break;
+      case 8:
+      case 7:
+      case 6:
+        mensajePuntaje='¡Bien hecho!';
+        break;
+      default:
+        mensajePuntaje='No te rindas, sigue intentandolo :)'
+        break;
     }
+
+    //Invoca al metodo que contiene los resultados
+    let results = this.obtenerHoja(this.random);
+
+
+
     //En esta variable se guarda el contenido que imprime el PDF
     const pdf:any = {
       content:[
         {
-          text:'¡Quizz de preguntas en la Luna!',
-        style:'header'
+          text:'Hoja de resultados:\n\n',
+          style:'header'
         },
         {
-        text:['Muchas felicidades\n',
+        text:[''+mensajePuntaje+'\n\n',
           'Haz obtenido una puntuación de: '+this.puntaje+'\n\n']
         },
         {
           text:['Hoja de Repuestas: \n\n',
-            '1.-¿Cual es el llamado Planeta Rojo? \n',
-            'Respuesta: Marte \n\n',
+            '1.-'+ results[10] + '\n',
+            'Respuesta: '+ results[0]+' \n\n',
 
-            '2.-¿Cual es el llamado Planeta Gigante Gaseoso? \n',
-            'Respuesta: Jupiter \n\n',
+            '2.-'+ results[11] + '\n',
+            'Respuesta: '+ results[1]+' \n\n',
 
-            '3.-¿Que es un año luz? \n',
-            'Respuesta: Es una unidad de medida en distancia utilizada en astronomía \n\n',
+            '3.-'+ results[12] + '\n',
+            'Respuesta: '+ results[2]+' \n\n',
 
-            '4.-¿Dónde termina el Sistema Solar? \n',
-            'Respuesta: Pasando la nube Oort \n\n',
+            '4.-'+ results[13] + '\n',
+            'Respuesta: '+ results[3]+' \n\n',
 
-            '5.-¿Cuál es la estrella más cercana a la tierra? \n',
-            'Respuesta: El sol \n\n',
+            '5.-'+ results[14] + '\n',
+            'Respuesta: '+ results[4]+' \n\n',
 
-            '6.-¿Cuál es la galaxia más cercana? \n',
-            'Respuesta: Enana del Can Mayor \n\n',
+            '6.-'+ results[15] + '\n',
+            'Respuesta: '+ results[5]+' \n\n',
 
-            '7.-¿De tipo de estrella es nuestro sol? \n',
-            'Respuesta: De tipo G \n\n',
+            '7.-'+ results[16] + '\n',
+            'Respuesta: '+ results[6]+' \n\n',
 
-            '8.-¿Cuál es la llamada Galaxia del pingüino? \n',
-            'Respuesta: NGC 2936 \n\n',
+            '8.-'+ results[17] + '\n',
+            'Respuesta: '+ results[7]+' \n\n',
 
-            '9.-¿Cuanto tarda la luna luna en dar una vuelta al rededor de la tierra? \n',
-            'Respuesta: 28 días \n\n',
+            '9.-'+ results[18] + '\n',
+            'Respuesta: '+ results[8]+' \n\n',
 
-            '10.-¿Que fenómenos del planeta tierra estan influenciados por la luna? \n',
-            'Respuesta: Las mareas y el clima \n\n\n',
+            '10.-'+ results[19] + '\n',
+            'Respuesta: '+ results[9]+' \n\n',
 
           ]
           },
+
         {
           image:this.lunaDatos[5].imagen,
-          width: 500
+          width: 460
         },
+
         {
           text: 'Plandi - Plataformas Educativas ©',
           style: ['quote', 'small']
@@ -182,7 +226,5 @@ export class LunaComponent implements OnInit{
     }
     pdfMake.createPdf(pdf).open();
   }
-
-/////////////////////////////////////////////
 
 }
