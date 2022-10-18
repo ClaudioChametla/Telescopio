eda79fdf8246fe25c9f55a51b6dd42d42af0c8a0
ClaudioChametla <clauchametla.777@gmail.com>
2022-10-18 18:01:19 -0500

    Añadidos estilos al quizz

--- src/app/components/luna/luna.component.ts
+++ src/app/components/luna/luna.component.ts
@@ -1,84 +1,87 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
-import luna from '../../files/luna.json'
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
+import luna from '../../files/luna.json';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-luna',
   templateUrl: './luna.component.html',
-  styleUrls: ['./luna.component.scss']
+  styleUrls: ['./luna.component.scss'],
 })
-
-
-export class LunaComponent implements OnInit{
-
+export class LunaComponent implements OnInit {
   //Variables del cuestionario
-  cuestionario:Boolean=false;
-  lunaDatos:any;
-  selector:number=0;
-  puntaje:number=0;
-  conteoPuntaje:number=0;
-  seguir:Boolean=false;
-  random:number=0;
+  cuestionario: Boolean = false;
+  lunaDatos: any;
+  selector: number = 0;
+  puntaje: number = 0;
+  conteoPuntaje: number = 0;
+  seguir: Boolean = false;
+  random: number = 0;
 
   //Para mostrar u ocultar la pizarra
-  hiddenGame:Boolean=false;
+  hiddenGame: Boolean = false;
 
-
-  constructor(private router:Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.lunaDatos=luna;
+    this.lunaDatos = luna;
     /**Selecciona un numero aleatorio entre 1 y 4
     Para seleccionar el cuestionario
     */
     this.random = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
     /* console.log(this.puntaje); */
-
   }
 
   /** Oculta la pizarra*/
-  salir(){
-    this.hiddenGame=false;
-    this.puntaje=0;
+  salir() {
+    this.hiddenGame = false;
+    this.puntaje = 0;
   }
 
   /** Muestra el cuestionario */
-  mostrarCuestionario(){
-    this.cuestionario=true;
-    this.puntaje=0;
-    this.selector=0;
+  mostrarCuestionario() {
+    this.cuestionario = true;
+    this.puntaje = 0;
+    this.selector = 0;
   }
 
   /** Elige y obtiene el valor de la opción A
   @param e Recibe por medio de un evento el valor de la pregunta
   */
-  seleccionarOpcion(e:any){
-    if(e==1){
-      this.conteoPuntaje=1;
-      this.seguir=true;
-    }else{
-      this.conteoPuntaje=0
-      this.seguir=true;
+  seleccionarOpcion(e: any) {
+    if (e == 1) {
+      this.conteoPuntaje = 1;
+      this.seguir = true;
+    } else {
+      this.conteoPuntaje = 0;
+      this.seguir = true;
     }
   }
 
   //Envia hacía la siguiente pregunta
-  siguientePregunta(){
+  siguientePregunta() {
     //Evalua si no es la ultima pregunta, son 10 preguntas en total
-    if(this.seguir===true){
-        this.puntaje+=this.conteoPuntaje;
-        /* console.log('Puntaje',this.puntaje); */
-      if(this.selector < 9){
-        this.selector=this.selector+1;
-        this.seguir=false;
+    if (this.seguir === true) {
+      this.puntaje += this.conteoPuntaje;
+      /* console.log('Puntaje',this.puntaje); */
+      if (this.selector < 9) {
+        this.selector = this.selector + 1;
+        this.seguir = false;
         /* console.log('Contador numero preguntas',this.selector); */
-      }else{
+      } else {
         Swal.fire({
-          title: '<strong>¡ Lo lograste! <u>Tu puntaje es de </u></strong>' + this.puntaje,
+          title:
+            '<strong>¡ Lo lograste! <u>Tu puntaje es de </u></strong>' +
+            this.puntaje,
           icon: 'info',
           showDenyButton: true,
           showCancelButton: false,
@@ -86,35 +89,33 @@ export class LunaComponent implements OnInit{
           denyButtonText: `Salir`,
         }).then((result) => {
           if (result.isConfirmed) {
-            Swal.fire('Impreso', '', 'success')
+            Swal.fire('Impreso', '', 'success');
             this.imprimirResultados();
-            this.cuestionario=false;
+            this.cuestionario = false;
           } else if (result.isDenied) {
-            this.salirCuestionario()
+            this.salirCuestionario();
           }
-        })
+        });
       }
-    }else{
+    } else {
       Swal.fire({
         icon: 'error',
         title: 'Oh no',
         text: 'Aun no haz elegido una respuesta',
-        footer: 'Elige una respuesta y vuelve a intentarlo'
-      })
+        footer: 'Elige una respuesta y vuelve a intentarlo',
+      });
     }
-
   }
 
   //Este metodo lleva al componente sistema-solar
-  irSistema(){
+  irSistema() {
     this.router.navigate(['/sistema-solar']);
   }
 
-   //Este metodo te muestra la pantalla de bienvenida del formulario
-  salirCuestionario(){
-    this.cuestionario=false;
+  //Este metodo te muestra la pantalla de bienvenida del formulario
+  salirCuestionario() {
+    this.cuestionario = false;
   }
-
 
   /**
    * @author ClaudioChametla
@@ -122,15 +123,15 @@ export class LunaComponent implements OnInit{
    * @returns devuelve en un arreglo las preguntas y respuestas
    * Metodo para obtener la hoja de respuestas */
 
-  private obtenerHoja(ran:number){
-    let answer=[];
+  private obtenerHoja(ran: number) {
+    let answer = [];
     for (const element of this.lunaDatos[ran]) {
-      if(element.opciones[0].value==1){
-        answer.push(element.opciones[0].opcion)
-      }else if(element.opciones[1].value==1){
-        answer.push(element.opciones[1].opcion)
-      } else if(element.opciones[2].value==1){
-        answer.push(element.opciones[2].opcion)
+      if (element.opciones[0].value == 1) {
+        answer.push(element.opciones[0].opcion);
+      } else if (element.opciones[1].value == 1) {
+        answer.push(element.opciones[1].opcion);
+      } else if (element.opciones[2].value == 1) {
+        answer.push(element.opciones[2].opcion);
       }
     }
     for (const element of this.lunaDatos[ran]) {
@@ -140,7 +141,7 @@ export class LunaComponent implements OnInit{
   }
 
   /**Este metodo crea un PDF con los resultados obtenidos*/
-  imprimirResultados(){
+  imprimirResultados() {
     let mensajePuntaje: string = '';
 
     //Si el puntaje obtenido es mayor a 9 desbloquea la pizarra
@@ -148,99 +149,97 @@ export class LunaComponent implements OnInit{
     switch (this.puntaje) {
       case 10:
       case 9:
-
-        this.hiddenGame=true;
-        mensajePuntaje='¡¡¡ Muchas felicidades !!!, Excelente trabajo';
+        this.hiddenGame = true;
+        mensajePuntaje = '¡¡¡ Muchas felicidades !!!, Excelente trabajo';
         break;
       case 8:
       case 7:
       case 6:
-        mensajePuntaje='¡Bien hecho!';
+        mensajePuntaje = '¡Bien hecho!';
         break;
       default:
-        mensajePuntaje='No te rindas, sigue intentandolo :)'
+        mensajePuntaje = 'No te rindas, sigue intentandolo :)';
         break;
     }
 
     //Invoca al metodo que contiene los resultados
     let results = this.obtenerHoja(this.random);
 
-
-
     //En esta variable se guarda el contenido que imprime el PDF
-    const pdf:any = {
-      content:[
+    const pdf: any = {
+      content: [
         {
-          text:'Hoja de resultados:\n\n',
-          style:'header'
+          text: 'Hoja de resultados:\n\n',
+          style: 'header',
         },
         {
-        text:[''+mensajePuntaje+'\n\n',
-          'Haz obtenido una puntuación de: '+this.puntaje+'\n\n']
+          text: [
+            '' + mensajePuntaje + '\n\n',
+            'Haz obtenido una puntuación de: ' + this.puntaje + '\n\n',
+          ],
         },
         {
-          text:['Hoja de Repuestas: \n\n',
-            '1.-'+ results[10] + '\n',
-            'Respuesta: '+ results[0]+' \n\n',
+          text: [
+            'Hoja de Repuestas: \n\n',
+            '1.-' + results[10] + '\n',
+            'Respuesta: ' + results[0] + ' \n\n',
 
-            '2.-'+ results[11] + '\n',
-            'Respuesta: '+ results[1]+' \n\n',
+            '2.-' + results[11] + '\n',
+            'Respuesta: ' + results[1] + ' \n\n',
 
-            '3.-'+ results[12] + '\n',
-            'Respuesta: '+ results[2]+' \n\n',
+            '3.-' + results[12] + '\n',
+            'Respuesta: ' + results[2] + ' \n\n',
 
-            '4.-'+ results[13] + '\n',
-            'Respuesta: '+ results[3]+' \n\n',
+            '4.-' + results[13] + '\n',
+            'Respuesta: ' + results[3] + ' \n\n',
 
-            '5.-'+ results[14] + '\n',
-            'Respuesta: '+ results[4]+' \n\n',
+            '5.-' + results[14] + '\n',
+            'Respuesta: ' + results[4] + ' \n\n',
 
-            '6.-'+ results[15] + '\n',
-            'Respuesta: '+ results[5]+' \n\n',
+            '6.-' + results[15] + '\n',
+            'Respuesta: ' + results[5] + ' \n\n',
 
-            '7.-'+ results[16] + '\n',
-            'Respuesta: '+ results[6]+' \n\n',
+            '7.-' + results[16] + '\n',
+            'Respuesta: ' + results[6] + ' \n\n',
 
-            '8.-'+ results[17] + '\n',
-            'Respuesta: '+ results[7]+' \n\n',
+            '8.-' + results[17] + '\n',
+            'Respuesta: ' + results[7] + ' \n\n',
 
-            '9.-'+ results[18] + '\n',
-            'Respuesta: '+ results[8]+' \n\n',
+            '9.-' + results[18] + '\n',
+            'Respuesta: ' + results[8] + ' \n\n',
 
-            '10.-'+ results[19] + '\n',
-            'Respuesta: '+ results[9]+' \n\n',
-
-          ]
-          },
+            '10.-' + results[19] + '\n',
+            'Respuesta: ' + results[9] + ' \n\n',
+          ],
+        },
 
         {
-          image:this.lunaDatos[5].imagen,
-          width: 460
+          image: this.lunaDatos[5].imagen,
+          width: 460,
         },
 
         {
           text: 'Plandi - Plataformas Educativas ©',
-          style: ['quote', 'small']
-        }
-    ],
+          style: ['quote', 'small'],
+        },
+      ],
       styles: {
         header: {
           fontSize: 18,
-          bold: true
+          bold: true,
         },
         subheader: {
           fontSize: 14,
-          bold: true
+          bold: true,
         },
         quote: {
-          italics: true
+          italics: true,
         },
         small: {
-          fontSize: 8
-        }
-      }
-    }
+          fontSize: 8,
+        },
+      },
+    };
     pdfMake.createPdf(pdf).open();
   }
-
 }
