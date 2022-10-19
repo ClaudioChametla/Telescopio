39b6b73670b168593209ae0b931cb53725b4f173
ClaudioChametla <clauchametla.777@gmail.com>
2022-10-19 14:03:38 -0500

    Modificado telescopio para responsividad

--- src/app/components/telescopio/telescopio.component.ts
+++ src/app/components/telescopio/telescopio.component.ts
@@ -1,62 +1,70 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  /*   HostListener,
+  ViewChild, */
+} from '@angular/core';
 
 @Component({
   selector: 'app-telescopio',
   templateUrl: './telescopio.component.html',
-  styleUrls: ['./telescopio.component.scss']
+  styleUrls: ['./telescopio.component.scss'],
 })
 export class TelescopioComponent implements OnInit {
+  @Input() datos: any;
+  /*   @ViewChild('telescopio', { static: false }) telescopio: any;
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.magnify('myImage');
+  }
+ */
+  //imgID = 'https://images8.alphacoders.com/601/601973.jpg';
 
-  @Input() datos:any;
-
-  imgID = "https://images8.alphacoders.com/601/601973.jpg";
-
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.magnify("myimage");
+    this.magnify('myimage');
   }
 
   magnify(imgID) {
-    let zoom = 3 ;
+    let zoom = 3;
     var img, glass, w, h, bw;
     img = document.getElementById(imgID);
+    console.log(img);
+
     /*Crea la lupa*/
-    glass = document.createElement("DIV");
-    glass.setAttribute("class", "img-magnifier-glass");
+    glass = document.createElement('DIV');
+    glass.setAttribute('class', 'img-magnifier-glass');
     /*Inserta la lupa*/
     img.parentElement.insertBefore(glass, img);
     /*set background properties for the magnifier glass:*/
-    glass.style.backgroundImage = "url('" + this.datos[0]+ "')";
-    glass.style.backgroundRepeat = "no-repeat";
+    glass.style.backgroundImage = "url('" + this.datos[0] + "')";
+    glass.style.backgroundRepeat = 'no-repeat';
     glass.style.backgroundSize =
-      img.width * zoom + "px " + img.height * zoom + "px";
+      img.width * zoom + 'px ' + img.height * zoom + 'px';
     bw = 3;
     w = glass.offsetWidth / 2;
     h = glass.offsetHeight / 2;
 
     //Se abre o se cierra el circulo
-    let open = document.getElementById("open")
-    open.addEventListener("click", abrir);
-    let close = document.getElementById("close")
-    close.addEventListener("click", cerrar);
-
+    let open = document.getElementById('open');
+    open.addEventListener('click', abrir);
+    let close = document.getElementById('close');
+    close.addEventListener('click', cerrar);
     //zoom
-    let aumentar = document.getElementById("aumentar");
-    aumentar.addEventListener("click",zoomMas)
-
-    let disminuir = document.getElementById("disminuir");
-    disminuir.addEventListener("click",zoomMenos)
-
+    let aumentar = document.getElementById('aumentar');
+    aumentar.addEventListener('click', zoomMas);
+    let disminuir = document.getElementById('disminuir');
+    disminuir.addEventListener('click', zoomMenos);
 
     /*execute a function when someone moves the magnifier glass over the image:*/
-    glass.addEventListener("mousemove", moveMagnifier);
-    img.addEventListener("mousemove", moveMagnifier);
+    glass.addEventListener('mousemove', moveMagnifier);
+    img.addEventListener('mousemove', moveMagnifier);
 
     /*and also for touch screens:*/
-    glass.addEventListener("touchmove", moveMagnifier);
-    img.addEventListener("touchmove", moveMagnifier);
+    glass.addEventListener('touchmove', moveMagnifier);
+    img.addEventListener('touchmove', moveMagnifier);
     function moveMagnifier(e) {
       var pos, x, y;
       /*prevent any other actions that may occur when moving over the image*/
@@ -79,11 +87,11 @@ export class TelescopioComponent implements OnInit {
         y = h / zoom;
       }
       /*set the position of the magnifier glass:*/
-      glass.style.left = x - w + "px";
-      glass.style.top = y - h + "px";
+      glass.style.left = x - w + 'px';
+      glass.style.top = y - h + 'px';
       /*display what the magnifier glass "sees":*/
       glass.style.backgroundPosition =
-        "-" + (x * zoom - w + bw) + "px -" + (y * zoom - h + bw) + "px";
+        '-' + (x * zoom - w + bw) + 'px -' + (y * zoom - h + bw) + 'px';
     }
     function getCursorPos(e) {
       var a,
@@ -101,47 +109,54 @@ export class TelescopioComponent implements OnInit {
       return { x: x, y: y };
     }
 
-    function abrir(){
-      glass.style.width="350px"
-      glass.style.height="350px"
+    function abrir() {
+      glass.style.width = '350px';
+      glass.style.height = '350px';
       glass.style.backgroundSize =
-      img.width * zoom + "px " + img.height * zoom + "px";
-      bw = 3;
-      w = glass.offsetWidth / 2;
-      h = glass.offsetHeight / 2;
-
-    }
-    function cerrar(){
-      glass.style.width="250px"
-      glass.style.height="250px"
-      glass.style.backgroundSize =
-      img.width * zoom + "px " + img.height * zoom + "px";
+        img.width * zoom + 'px ' + img.height * zoom + 'px';
       bw = 3;
       w = glass.offsetWidth / 2;
       h = glass.offsetHeight / 2;
     }
-
-    function zoomMas(){
-      zoom=4;
+    function cerrar() {
+      glass.style.width = '250px';
+      glass.style.height = '250px';
       glass.style.backgroundSize =
-      img.width * zoom + "px " + img.height * zoom + "px";
+        img.width * zoom + 'px ' + img.height * zoom + 'px';
       bw = 3;
       w = glass.offsetWidth / 2;
       h = glass.offsetHeight / 2;
     }
 
-
-    function zoomMenos(){
-      zoom=2;
+    function zoomMas() {
+      zoom = 4;
       glass.style.backgroundSize =
-      img.width * zoom + "px " + img.height * zoom + "px";
+        img.width * zoom + 'px ' + img.height * zoom + 'px';
       bw = 3;
       w = glass.offsetWidth / 2;
       h = glass.offsetHeight / 2;
     }
 
+    function zoomMenos() {
+      zoom = 2;
+      glass.style.backgroundSize =
+        img.width * zoom + 'px ' + img.height * zoom + 'px';
+      bw = 3;
+      w = glass.offsetWidth / 2;
+      h = glass.offsetHeight / 2;
+    }
+
+    /**Detecta el cambio de tamaño de la pantalla y vuelve
+     * a ajutar el tamaño de la imagen de la lupa
+     * @return void
+     */
+    window.onresize = function resize() {
+      console.log('changing . . .');
+      glass.style.backgroundSize =
+        img.width * zoom + 'px ' + img.height * zoom + 'px';
+      bw = 3;
+      w = glass.offsetWidth / 2;
+      h = glass.offsetHeight / 2;
+    };
   }
-
-
-
 }
