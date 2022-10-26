386888a0731efc1ad4d57253b55a229720d6bdcb
ClaudioChametla <clauchametla.777@gmail.com>
2022-10-26 17:20:09 -0500

    Modificado estilo de componente mi estrella

--- src/app/components/mi-estrella/mi-estrella.component.ts
+++ src/app/components/mi-estrella/mi-estrella.component.ts
@@ -12,42 +12,70 @@ export class MiEstrellaComponent implements OnInit {
   //Json imagenes de estrellas y frase de atrofisicos
   miEstrellaDatos: any = miEstrella;
   nombreEstrella: String = '';
+  nombre: String = '';
   miEstrella: Boolean = false;
   mostrarEstrella: Boolean = false;
   rand: Number = 0;
 
   modalState: Boolean = false;
 
+  lockKey: Boolean = false;
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
-  //Muestra la estrella
-  mostrarMiEstrella() {
-    this.miEstrella = true;
-  }
-
   //Recibe el nombre de la estrella lo valida
   //Seleccionara un numero aleatorio y mostrara la estrella
   irEstrella() {
+    let input = document.getElementById('input-nombre');
     if (this.nombreEstrella === '') {
-      Swal.fire({
-        icon: 'error',
-        title: 'Ups',
-        text: 'Aun no se ha escrito nada',
-      });
+      input.classList.add('is-invalid');
     } else {
-      this.miEstrella = false;
-      this.mostrarEstrella = true;
-      this.rand = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
+      this.nombre = this.nombreEstrella;
+      input.classList.remove('is-invalid');
+      input.classList.add('is-valid');
+      this.lockKey = true;
+      this.cerrarModal();
+
+      setTimeout(() => {
+        input.classList.remove('is-valid');
+        this.frase();
+        this.lockKey = false;
+      }, 500);
     }
   }
-  //Mesyta el componente estrellas
+
+  frase() {
+    this.mostrarEstrella = true;
+    this.rand = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
+  }
+  salirFrase() {
+    let modal = document.getElementById('modal-nombre');
+    modal.classList.remove('animate__fadeInDown');
+    modal.classList.add('animate__backOutUp');
+    setTimeout(() => {
+      modal.classList.remove('animate__backOutUp');
+      this.mostrarEstrella = false;
+    }, 1000);
+  }
+
+  //Muesta el componente estrellas
   irEstrellas() {
     this.router.navigate(['/estrellas']);
   }
 
   mostrarModal() {
     this.modalState = !this.modalState;
+  }
+  cerrarModal() {
+    let modal = document.getElementById('modal-nombre');
+    modal.classList.remove('animate__fadeInDown');
+    modal.classList.add('animate__bounceOutUp');
+    setTimeout(() => {
+      modal.classList.remove('animate__bounceOutUp');
+      this.nombreEstrella = '';
+      this.modalState = !this.modalState;
+    }, 1000);
   }
 }
