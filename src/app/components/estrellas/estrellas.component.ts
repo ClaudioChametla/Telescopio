2435ddb586c3686061d69cb216918aca79e15d92
ClaudioChametla <clauchametla.777@gmail.com>
2022-10-28 13:19:29 -0500

    Agregado nuevas imagenes y nuevo JSON misestrellas para mostrar nombre de estrellas

--- src/app/components/estrellas/estrellas.component.ts
+++ src/app/components/estrellas/estrellas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import estrellas from '../../files/estrellas.json';
+import misestrellas from '../../files/misestrellas.json';
 import { CctServiceService } from 'src/app/service/cct-service.service';
 import { Router } from '@angular/router';
 
@@ -12,6 +13,9 @@ export class EstrellasComponent implements OnInit {
   mostrarEstrellas = true;
   estrellasDatos: any;
   estrellasSelec: any;
+  nombre: string = 'Mi estrella';
+  randNumber: number = 0;
+  typeStar: any;
 
   constructor(private router: Router, private service: CctServiceService) {}
 
@@ -20,6 +24,21 @@ export class EstrellasComponent implements OnInit {
       this.mostrarEstrellas = res;
     });
     this.estrellasDatos = estrellas;
+    this.revisarMiEstrella();
+  }
+
+  revisarMiEstrella() {
+    this.nombre = localStorage.getItem('nombre');
+    let rand = localStorage.getItem('rand');
+
+    if (this.nombre == undefined || rand == undefined) {
+      this.nombre = 'Mi estrella';
+      this.typeStar = misestrellas[7];
+    } else {
+      this.randNumber = Number(rand);
+      this.typeStar = misestrellas[this.randNumber];
+      console.log(this.typeStar);
+    }
   }
 
   irSistema() {
